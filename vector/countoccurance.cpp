7a5eba57eb3d1e7aa47f1c4d99c98116277a0895
ajju <azadbharati802223@gmail.com>
2025-10-14 16:36:49 +0530

    vector

---
+++ vector/countoccurance.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of vector:- ";
+    cin>>n;
+    vector <int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+     int x;
+    cout<<"Enter the count number:- ";
+    cin>>x;
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(v[i]==x){
+            count++;
+        }
+    }
+    cout<<"The final count is "<<count;
+}
